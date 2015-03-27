602751f494da2673c12ebc90eadbe6e4b04fa605
Gianni Moschini <gianni.moschini@hailocab.com>
2015-03-27 23:25:22 +0000

    Goformat and add a second item

--- main.go
+++ main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-  "encoding/json"
+	"encoding/json"
 
-  "github.com/go-martini/martini"
-  "github.com/leibowitz/halgo"
+	"github.com/go-martini/martini"
+	"github.com/leibowitz/halgo"
 )
 
 type MyItem struct {
-  halgo.Links
-  Reference string `json:"reference"`
+	halgo.Links
+	Reference string `json:"reference"`
 }
 
 type MyResource struct {
-  halgo.Links
-  halgo.Embedded
-  Name string `json:"name"`
+	halgo.Links
+	halgo.Embedded
+	Name string `json:"name"`
 }
 
 func main() {
-  m := martini.Classic()
-  m.Get("/", func() string {
-    res := MyResource{
-      Links: halgo.Links{}.
-        Self("/orders").
-        Next("/orders?page=2").
-        Link("ea:find", "/orders{?id}").
-        Add("ea:admin", halgo.Link{Href: "/admins/2", Title: "Fred"}, halgo.Link{Href: "/admins/5", Title: "Kate"}),
-      Embedded: halgo.Embedded{}.Add("ea:order", MyItem{Reference: "ABC"}),
-      Name: "James",
-    }
-    bytes, err := json.Marshal(res)
-    if err != nil {
-      return err.Error()
-    }
-    return string(bytes)
-  })
-  m.RunOnAddr(":8080")
+	m := martini.Classic()
+	m.Get("/", func() string {
+		res := MyResource{
+			Links: halgo.Links{}.
+				Self("/orders").
+				Next("/orders?page=2").
+				Link("ea:find", "/orders{?id}").
+				Add("ea:admin", halgo.Link{Href: "/admins/2", Title: "Fred"}, halgo.Link{Href: "/admins/5", Title: "Kate"}),
+			Embedded: halgo.Embedded{}.Add("ea:order", MyItem{Reference: "ABC"}, MyItem{Reference: "DEF"}),
+			Name:     "James",
+		}
+		bytes, err := json.Marshal(res)
+		if err != nil {
+			return err.Error()
+		}
+		return string(bytes)
+	})
+	m.RunOnAddr(":8080")
 }
